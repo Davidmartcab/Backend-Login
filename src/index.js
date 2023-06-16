f4e1360e3008fbfa7528ffc2c7f3283e04cbbfcf
dmartinez2 <dmartinez@zalcu.com>
2023-06-16 12:47:04 +0200

    Token expire in 1h

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import app from "./app.js";
 import { connectDB } from "./db.js";
 
 
+
 app.listen(process.env.PORT, () => {
     console.log(`>>> Server running on port: ${process.env.PORT}`)
     connectDB()
