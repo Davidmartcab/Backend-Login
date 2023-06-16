f0b278faeb43abb810f6d3f8124fd695d9ba629b
dmartinez2 <dmartinez@zalcu.com>
2023-06-16 11:46:37 +0200

    It take the token from cookies or from header

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const authRequired = async (req, res, next) => {
 
-    const token = req.cookies.token;
+    const token = req.cookies.token || req.headers.token;
 
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
